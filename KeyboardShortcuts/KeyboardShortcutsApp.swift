aa3f3b96c88d6b60a243842e3f1082eeab3ab219
Russell Gordon <rgordon@lcs.on.ca>
2022-11-20 08:11:22 -0500

    Initial Commit

---
+++ KeyboardShortcuts/KeyboardShortcutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyboardShortcutsApp.swift
+//  KeyboardShortcuts
+//
+//  Created by Russell Gordon on 2022-11-20.
+//
+
+import SwiftUI
+
+@main
+struct KeyboardShortcutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
